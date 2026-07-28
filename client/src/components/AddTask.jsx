755d6acd222a235decf7fb27db01109e6a42c3c2
Deivson Albuquerque <cavalcantemilton042@gmail.com>
2026-07-28 14:57:04 +0000

    alteracao botao tarefa

--- client/src/components/AddTask.jsx
+++ client/src/components/AddTask.jsx
@@ -59,7 +59,7 @@ const AddTask = ({ onAdd }) => {
       </div>
       
       <button type="submit" className="btn btn-block success">
-        Adicionar Task
+        Adicionar Task old
       </button>
       
       <Modal
